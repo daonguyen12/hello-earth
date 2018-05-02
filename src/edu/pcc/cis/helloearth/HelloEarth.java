8eb2fb78566a0e5437a314543104537cff04dee0
unknown <Dao@ASUS-Lite.com>
2018-05-01 22:49:25 -0700

    added printName method

--- src/edu/pcc/cis/helloearth/HelloEarth.java
+++ src/edu/pcc/cis/helloearth/HelloEarth.java
@@ -8,4 +8,8 @@ public class HelloEarth {
     public void printHowAreYou(){
         System.out.println("How are you?");
     }
+
+    public void printName(String name){
+        System.out.println("Hello World" + name);
+    }
 }
